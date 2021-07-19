006c310c3f77a1d3981b76c9aff1740ca88ccef7
Katherine Eaton <ktmeaton@gmail.com>
2021-07-19 13:40:10 -0400

    successful run of experience

--- resume.py
+++ resume.py
@@ -11,10 +11,12 @@ template_dir = os.path.join(cwd, "templates")
 sections = [
     "summary",
     "education",
+    "experience",
 ]
 
 
 for section in sections:
+    print(section)
     template = importlib.import_module("templates." + section)
     metadata_path = os.path.join(metadata_dir, section + ".tsv")
     out_path = os.path.join("resume", section + ".tex")
@@ -26,6 +28,16 @@ for section in sections:
         # Check for newline problems
         if metadata[-1] == ['']:
             metadata = metadata[:-1]
+
+        # Format bullets
+        if "bullets" in header:
+            for entry in metadata:
+                bullets_raw = entry[header.index("bullets")]
+                bullets_split = bullets_raw.split(";")
+                bullets_str = ["\item {" + val + "}" for val in bullets_split]
+                bullets = "\n\t\t\t".join(bullets_str)
+
+                entry[header.index("bullets")] = bullets
 
     # Write to the output resume directory
     with open(out_path, "w") as outfile:
